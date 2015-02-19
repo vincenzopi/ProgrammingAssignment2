d3abccf90d83859c30664b6ba042e9a5d6cebab9
vincenzopi <vincenzopi@gmail.com>
2015-02-19 20:21:45 +0100

    Commit MakeCacheMatrix fnc

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -2,3 +2,21 @@
 #to caching the inverse of a matrix rather than compute it repeatedly.
 
 #Function makeCacheMatrix creates a matrix object that can cache its inverse
+
+makeCacheMatrix<- function(x = matrix()) {
+  my_out <- NULL
+  set <- function(y) {
+    x <<- y
+    my_out <<- NULL
+  }
+  get <- function() x
+  #
+  #Define set
+  setInvMtr <- function(inv_matrix) my_out <<- inv_matrix
+  #Define get
+  getInvMtr <- function() my_out
+  #
+  list(set = set, get = get,
+       setInvMtr = setInvMtr,
+       getInvMtr = getInvMtr)
+}
